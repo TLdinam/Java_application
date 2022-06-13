9fcfaf561de6920445397ee38af327b447cc174d
TLdinam <tarasl@ven.com>
2022-06-13 16:34:35 +0300

    First version

---
+++ src/main/java/Man.java
@@ -0,0 +1,6 @@
+public class Man extends Person{
+
+    public Man(String firstName, String lastName, int age, String partnerName, String partnerLastName, String gender) {
+        super(firstName, lastName, age, partnerName, partnerLastName, gender);
+    }
+}
